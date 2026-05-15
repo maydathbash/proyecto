c9ac0cf7535efd7727e5fe81a3c43a6d3a4f12ad
maydathbash <sergiodelreal02@gmail.com>
2026-05-15 14:26:47 +0200

    Delete backup directory

--- backup/cooking/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CookingConfig(AppConfig):
-    name = 'cooking'
--- backup/cooking/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from cooking.models import Showcooking, Receta
-from django.db.models import Avg, Count
-
-
-# Create your views here.
-
